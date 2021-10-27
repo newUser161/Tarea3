38cbc0ebd413d7a3d3bbf2bf9315382598f582dc
IvoRivera <ivorivera94@gmail.com>
2021-10-27 01:48:34 -0300

    avance separacion en modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { DetalleNoticiaComponent } from './componentes/detalle-noticia/detalle-noticia.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { NoticiasComponent } from './componentes/noticias/noticias.component';
+import { ServicioAlClienteComponent } from './componentes/servicio-al-cliente/servicio-al-cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     DetalleNoticiaComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoticiasComponent,
+    ServicioAlClienteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/servicio-al-cliente/servicio-al-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servicio-al-cliente',
+  templateUrl: './servicio-al-cliente.component.html',
+  styleUrls: ['./servicio-al-cliente.component.scss']
+})
+export class ServicioAlClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
